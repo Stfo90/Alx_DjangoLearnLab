54f090bf0c5b99d97a23bdd565793ad3c162137f
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-11-12 23:41:14 +0100

    the add of relationship app and models and query samples

---
+++ Introduction_to_Django/django-models/bookshelf/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Fields to display in the list view
+    search_fields = ('title', 'author')                    # Fields to search by
+    list_filter = ('publication_year',)                    # Filter options
