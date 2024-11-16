a1323675a1b91bf62ba1344ac60af3d0da430dbe
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-11-16 23:52:24 +0100

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,6 +1,14 @@
+
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('relationship_app/', include('relationship_app.urls')),  # Include URLs from relationship_app
+ 
+   path('admin/', admin.site.urls),
+   path('relationship_app/', include('relationship_app.urls')),  # Include URLs from relationship_app
     # ... other project-level URLs
 ]
+
+
+
+
