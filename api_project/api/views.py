2771a0b0704c4c676bc9e3911de19f65c2a1daf2
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-11-24 23:53:48 +0100

    Implement authentication and permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import BasePermission
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  
     serializer_class = BookSerializer
@@ -13,3 +20,25 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+#Allows access only to authenticated usees.
+
+class SecureDataView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": f"Hello, {request.user}!"})
+# Restricts access to admin users.
+class AdminOnlyView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        return Response({"message": "Welcome, Admin!"})
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+
+
