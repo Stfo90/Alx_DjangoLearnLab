a1793dd2d06fb106dca589b6b83b2526cc4ecbe0
Mustapha Khamiz <mr.khamizmustapha@gmail.com>
2024-12-06 08:55:21 +0100

    comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     path('post/<int:post_id>/comment/', views.add_comment, name='add-comment'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),
 ]
